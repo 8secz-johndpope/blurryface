b108345d7efc615203e23e199d17a320a0c854f8
koratana <koratana@nock.ai>
2019-05-24 15:22:03 -0700

    added module abstraction to the networks

--- embeddings.py
+++ embeddings.py
@@ -15,59 +15,50 @@ import torch.nn.functional as F
 
 cudnn.benchmark = True
 
-model = None
-cuda_flag = None
-# img_transforms = None
+
+class EmbeddingGen(torch.nn.Module):
+    def __init__(self, model_type="LightCNN_9Layers"):
+        super(EmbeddingGen, self).__init__()
+        assert model_type in ["LightCNN_9Layers", "LightCNN_29Layers_v2"]
+
+        if model_type == "LightCNN_9Layers":
+            self.model = LightCNN_9Layers()
+        else:
+            self.model = LightCNN_29Layers_v2()
+
+    def _diff_grayscale(self, img):
+        R = img[:, 0, :, :]
+        G = img[:, 1, :, :]
+        B = img[:, 2, :, :]
+        return (0.299 *R + 0.587 *G + 0.114 *B).view(-1, 1, 128, 128) # im interpolating the images like they do in CV2 which was used to preprocess the images
+
+    def forward(self, images):
+        """
+        :param images: a tensor of shape (batch, channels, image_size, image_size)
+        :return: features: returns a (batch, 256) tensor of features
+        """
+        transformed = F.interpolate(images, size=(128, 128))
+        transformed = self._diff_grayscale(transformed)
+        if cuda_flag:
+            transformed.cuda()
+        return model.module.get_features(transformed)
+
+    @classmethod
+    def build(cls, model_type="LightCNN_9Layers", cuda=True, checkpoint="LightCNN_9Layers_checkpoint.pth.tar"):
+        global model, img_transforms, cuda_flag
+
+        model = cls(model_type)
+        model = torch.nn.DataParallel(model)
+        if cuda:
+            model = model.cuda()
+
+        print("=> loading face embedding checkpoint '{}'".format(checkpoint))
+        checkpoint = torch.load(checkpoint) if cuda else torch.load(checkpoint, map_location='cpu')
+
+        model.load_state_dict(checkpoint['state_dict'])
+        model.eval()
+        return model
 
 
-def init(model_type="LightCNN_9Layers", checkpoint=None, cuda=True):
-    assert model_type in ["LightCNN_9Layers", "LightCNN_29Layers_v2"]
-    global model, img_transforms, cuda_flag
-
-    if not checkpoint:
-        checkpoint = model_type + "_checkpoint.pth.tar"
-
-    if model_type == "LightCNN_9Layers":
-        model = LightCNN_9Layers()
-    else:
-        model = LightCNN_29Layers_v2()
-    model.eval()
-    model = torch.nn.DataParallel(model)
-    if cuda:
-        model = model.cuda()
-        cuda_flag = cuda
-
-    print("=> loading face embedding checkpoint '{}'".format(checkpoint))
-    if cuda:
-        checkpoint = torch.load(checkpoint)
-    else:
-        checkpoint = torch.load(checkpoint, map_location='cpu')
-    model.load_state_dict(checkpoint['state_dict'])
-
-    # img_transforms = transforms.Compose([
-    #     transforms.Grayscale(),
-    #     transforms.Resize((128, 128))
-    # ])
-
-
-def _diff_grayscale(img):
-    R = img[:, 0, :, :]
-    G = img[:, 1, :, :]
-    B = img[:, 2, :, :]
-    return (0.299 *R + 0.587 *G + 0.114 *B).view(-1, 1, 128, 128) # im interpolating the images like they do in CV2 which was used to preprocess the images
-
-
-def get_features(images):
-    """
-    :param images: a tensor of shape (batch, channels, image_size, image_size)
-    :return: features: returns a (batch, 256) tensor of features
-    """
-    global model, cuda_flag
-    assert model
-    transformed = F.interpolate(images, size=(128,128))
-    transformed = _diff_grayscale(transformed)
-    if cuda_flag:
-        transformed.cuda()
-    return model.module.get_features(transformed)
 
 
